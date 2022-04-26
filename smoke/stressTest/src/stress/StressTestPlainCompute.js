4e73e4b10eacc26c0c80b946b031014e1d778b74
babreu-ncsa <babreu@illinois.edu>
2022-04-26 07:24:50 -0500

    re-insert batch job calls

--- smoke/stressTest/src/stress/StressTestPlainCompute.js
+++ smoke/stressTest/src/stress/StressTestPlainCompute.js
@@ -1,13 +1,13 @@
 /**
- * Function call Plain Computation in the BatchQueue
+ * Function to call Plain Computation test in the BatchQueue
  * @param nBatches: number of batches to be sent
  * @param batchSize: number of calculations in each batch
  *
  * @return: BatchJob object
  */
 function runBatch(nBatches, batchSize) {
-    var jobOptions = StressTestJobOptions.make({nBatches: nBatches, batchSize: batchSize});
-    var job = StressTestJob.make({options: jobOptions}).upsert();
+    var jobOptions = StressTestPCJobOptions.make({nBatches: nBatches, batchSize: batchSize});
+    var job = StressTestPCJob.make({options: jobOptions}).upsert();
     job.start();
     return job;
 }
