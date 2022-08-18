d9017cd880125c13405d55d49cadcfdd9582384a
Bruno Abreu <babreu@illinois.edu>
2022-08-18 14:18:28 -0500

    some buggy version conflict...

--- smoke/smokeApp/src/entity/observations/ObservationOutputFile.py
+++ smoke/smokeApp/src/entity/observations/ObservationOutputFile.py
@@ -210,17 +210,14 @@ def upsertATOMData(this):
     parent_id = "OOS_SetName_" + obsSet.name + "_Ver_" + obsSet.versionTag
     df['parent'] = parent_id
 
-    #zero_time = datetime(1970,1,1,0,0)
-    #now_time = datetime.now()
-    #diff_time = (now_time - zero_time)
-    #versionTag= -1 * diff_time.total_seconds()
-    #df['dataVersion'] = versionTag
-
     output_records = df.to_dict(orient="records")
 
     # upsert this batch
     c3.ObservationAtomOutput.upsertBatch(objs=output_records)
 
-    this.processed = True
-    c3.ObservationOutputFile.merge(this)
+    c3.ObservationOutputFile(
+                id=this.id, 
+                processed=True
+    ).merge()
+
     return True
