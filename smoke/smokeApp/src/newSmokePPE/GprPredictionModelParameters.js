35326f7cfefd39bec550a3a9d9b6e23b7a4ac29a
Darren Adams <dadams@illinois.edu>
2023-12-11 16:57:44 -0600

    fix var #40

--- smoke/smokeApp/src/newSmokePPE/GprPredictionModelParameters.js
+++ smoke/smokeApp/src/newSmokePPE/GprPredictionModelParameters.js
@@ -1,6 +1,6 @@
 function getSerializedKernel(){
     var typeName = this.type().typeName();
     var thisType = TypeRef.make({"typeName": typeName}).toType();
-    thosObj = thisType.fetch({"filter":Filter.eq('id',this.id),"include":"serializedKernel"}).objs[0];
-    return thosObj.serializedKernel;
+    var thisObj = thisType.fetch({"filter":Filter.eq('id',this.id),"include":"serializedKernel"}).objs[0];
+    return thisObj.serializedKernel;
 }
