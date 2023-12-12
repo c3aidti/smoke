c5fafddc622e77e113a37995b56a94bb2156da45
Darren Adams <dadams@illinois.edu>
2023-12-12 10:13:30 -0600

    Remove copyright comments from TrainGprPredictionModelJob.js

--- smoke/smokeApp/src/newSmokePPE/batchJobs/TrainGprPredictionModelJob.js
+++ smoke/smokeApp/src/newSmokePPE/batchJobs/TrainGprPredictionModelJob.js
@@ -2,10 +2,6 @@
 * Copyright (c) 2022-2024, C3 AI DTI, Development Operations Team
 * All rights reserved. License: https://github.com/c3aidti/.github
 **/
-/**
-* Copyright (c) 2022-2024, C3 AI DTI, Development Operations Team
-* All rights reserved. License: https://github.com/c3aidti/.github
-**/
 
 function doStart(job, options) {
     // var dsetObj = SimulationEnsembleDataset.fetch({
