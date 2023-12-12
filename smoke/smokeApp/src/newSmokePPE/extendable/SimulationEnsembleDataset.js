0ae960f5183601cd3bd46731baafe58f7cef786c
Darren Adams <dadams@illinois.edu>
2023-12-12 12:20:43 -0600

    Fix composite key generation in SimulationEnsembleDataset.js

--- smoke/smokeApp/src/newSmokePPE/extendable/SimulationEnsembleDataset.js
+++ smoke/smokeApp/src/newSmokePPE/extendable/SimulationEnsembleDataset.js
@@ -123,9 +123,14 @@ function stageTrainedPredictionModelRowsForTechnique(geoTimeGridFetchSpec,techni
 
   var batch = [];
   var upcount = 0;
+  // Create a composite key for the upsert
+  var compositeKey = this.id + "_" + technique.id + "_" + gridPoint.id + "_";
+
   while(gridPoints.hasNext()) {
     var gridPoint = gridPoints.next();
+    var compositeKey = StringUtil.safeId(this.id + "_" + technique.id + "_" + gridPoint.id + "_");
     var row = modelType.make({
+      "id": compositeKey,
       "dataset": this,
       "geoTimeGridPoint": gridPoint,
       "technique": technique
