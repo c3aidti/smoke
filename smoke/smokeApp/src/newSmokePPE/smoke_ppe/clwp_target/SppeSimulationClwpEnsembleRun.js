6d4b8a9db1dc8c8169627d4d7327b5b66d9de103
vasanchez16 <vsanchez@andrew.cmu.edu>
2023-12-21 12:23:00 -0800

    get all clwp files necessary #41

--- smoke/smokeApp/src/newSmokePPE/smoke_ppe/clwp_target/SppeSimulationClwpEnsembleRun.js
+++ smoke/smokeApp/src/newSmokePPE/smoke_ppe/clwp_target/SppeSimulationClwpEnsembleRun.js
@@ -13,11 +13,29 @@ function upsertFileTable() {
    // SMOKE-PPE CONTAINER
     var containerRoot = FileSystem.urlFromMountAndRelativeEncodedPath("SMK_PPE");
     var pathToFiles = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_atmosphere";
+    var pathToFiles_air = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_air";
+    var pathToFiles_mf = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_mass_fraction_of_cloud_liquid_water_in_air";
     var fileStream = FileSystem.inst().listFilesStream(pathToFiles,-1);
+    var fileStream_air = FileSystem.inst().listFilesStream(pathToFiles_air,-1);
+    var fileStream_mf = FileSystem.inst().listFilesStream(pathToFiles_mf,-1);
     var smokePPEFiles = new Array();
 
     while(fileStream.hasNext()) {
         var file = fileStream.next();
+        if(file.url.endsWith(".nc")) {
+            smokePPEFiles.push(file);
+        };
+    };
+
+    while(fileStream_air.hasNext()) {
+        var file = fileStream_air.next();
+        if(file.url.endsWith(".nc")) {
+            smokePPEFiles.push(file);
+        };
+    };
+
+    while(fileStream_mf.hasNext()) {
+        var file = fileStream_mf.next();
         if(file.url.endsWith(".nc")) {
             smokePPEFiles.push(file);
         };
