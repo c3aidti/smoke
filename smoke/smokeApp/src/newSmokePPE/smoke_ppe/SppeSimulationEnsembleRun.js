aa2a73280909d8faf4021b72689c0897774f354c
vasanchez16 <vsanchez@andrew.cmu.edu>
2024-03-01 14:42:42 -0500

    update to upserting of output files #47

--- smoke/smokeApp/src/newSmokePPE/smoke_ppe/SppeSimulationEnsembleRun.js
+++ smoke/smokeApp/src/newSmokePPE/smoke_ppe/SppeSimulationEnsembleRun.js
@@ -18,13 +18,16 @@ function upsertFileTable() {
     var pathToFiles_exc = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s02i530_m01s02i530";
     var pathToFiles_cdnc_ctw = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s01i298_m01s01i298";
     var pathToFiles_cdnc_wghts = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s01i299_m01s01i299";
+    var pathToFiles_swrf = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s01i205_m01s01i205";
     var fileStream = FileSystem.inst().listFilesStream(pathToFiles,-1);
     var fileStream_air = FileSystem.inst().listFilesStream(pathToFiles_air,-1);
     var fileStream_mf = FileSystem.inst().listFilesStream(pathToFiles_mf,-1);
     var fileStream_exc = FileSystem.inst().listFilesStream(pathToFiles_exc,-1);
     var fileStream_cdnc_ctw = FileSystem.inst().listFilesStream(pathToFiles_cdnc_ctw,-1);
     var fileStream_cdnc_wghts = FileSystem.inst().listFilesStream(pathToFiles_cdnc_wghts,-1);
+    var fileStream_swrf = FileSystem.inst().listFilesStream(pathToFiles_swrf,-1);
     var smokePPEFiles = new Array();
+    var swrfFiles = new Array();
 
     while(fileStream.hasNext()) {
         var file = fileStream.next();
@@ -68,8 +71,18 @@ function upsertFileTable() {
         };
     };    
 
+    while(fileStream_swrf.hasNext()) {
+        var file = fileStream_swrf.next();
+        if(file.url.endsWith(".nc")) {
+            swrfFiles.push(file);
+        };
+    };
+
     var fileObjects = smokePPEFiles.map(createSimOutFile);
     SppeSimulationEnsembleOutputFile.upsertBatch(fileObjects);
+
+    var fileSwrfObjects = swrfFiles.map(createSwrfOutFile);
+    SppeSimulationEnsembleSwrfFile.upsertBatch(fileSwrfObjects)
 
     return 0;
  
@@ -78,6 +91,21 @@ function upsertFileTable() {
             var container = "smoke-ppe";
             var filename = file.url.split("azure://smoke-ppe/")[1];
             return SppeSimulationEnsembleOutputFile.make({
+                "id": filename,
+                "simulationRun": simSample,
+                "file": File.make({
+                    "url": file.url
+                }),
+                "container": container
+            });
+        };
+    };
+
+    function createSwrfOutFile(file) {
+        if (file.url.includes("azure://smoke-ppe/")) {
+            var container = "smoke-ppe";
+            var filename = file.url.split("azure://smoke-ppe/")[1];
+            return SppeSimulationEnsembleSwrfFile.make({
                 "id": filename,
                 "simulationRun": simSample,
                 "file": File.make({
