cedb9798e75481cdb3ca3df4a28655d9bf94ef64
Darren Adams <dadams@illinois.edu>
2024-03-26 12:30:53 -0500

    transform update#48

--- smoke/smokeApp/src/newSmokePPE/source/TransformSourceInterpolatedExtinctionCoeffToInterpFlightSimulationOutput.js
+++ smoke/smokeApp/src/newSmokePPE/source/TransformSourceInterpolatedExtinctionCoeffToInterpFlightSimulationOutput.js
@@ -1,16 +1,17 @@
-function transform(source){
+function transform(source) {
     var types = [];
     // For each simulation number, read the field: ens_<sim#>_m01s02i530_550nm
     // and appedn types array with json of new type
-    for (var i = 0; i <= 120; i++){
+    for (var i = 0; i <= 120; i++) {
         var field = "ens_" + i + "_m01s02i530_550nm";
-        types.push({
-            "type": "InterpolatedFlightSimulationOutput",
-            "simulationRun": {"id": "smoke_ppe_tatz_" + i},
-            "dataset" : {"id": "smoke_ppe_tatz_flight"},
-            "geoTimeGridPoint":{"id": source.latitude + "_" + source.longitude + "_" + source.time},
-            "exCoeff550": source[field]
-        });
+        types.push(
+            InterpolatedFlightSimulationOutput.make({
+                simulationRun: "smoke_ppe_tatz_" + i,
+                dataset: "smoke_ppe_tatz_flight",
+                geoTimeGridPoint: source.latitude + "_" + source.longitude + "_" + source.time,
+                exCoeff550: source[field]
+            })
+        );
 
     }
     return types;
