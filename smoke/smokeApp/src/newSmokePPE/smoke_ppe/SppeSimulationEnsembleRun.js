0aa38190d59e0364cb4c8992ebbc00c03a8e5100
Darren Adams <dadams@illinois.edu>
2024-04-10 07:36:52 -0500

    add stashCode to output file records #48

--- smoke/smokeApp/src/newSmokePPE/smoke_ppe/SppeSimulationEnsembleRun.js
+++ smoke/smokeApp/src/newSmokePPE/smoke_ppe/SppeSimulationEnsembleRun.js
@@ -19,7 +19,7 @@ function upsertFileTable() {
     var pathToFiles_cdnc_ctw = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s01i298_m01s01i298";
     var pathToFiles_cdnc_wghts = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s01i299_m01s01i299";
     var pathToFiles_swrf = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_toa_outgoing_shortwave_flux_m01s01i205";
-    var pathToFiles_extCoeff = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s02i530_m01s02i530";
+    // var pathToFiles_extCoeff = containerRoot + "ens_" + String(this.simulationNumber) + "_glm_m01s02i530_m01s02i530";
     var fileStream = FileSystem.inst().listFilesStream(pathToFiles,-1);
     var fileStream_air = FileSystem.inst().listFilesStream(pathToFiles_air,-1);
     var fileStream_mf = FileSystem.inst().listFilesStream(pathToFiles_mf,-1);
@@ -27,7 +27,7 @@ function upsertFileTable() {
     var fileStream_cdnc_ctw = FileSystem.inst().listFilesStream(pathToFiles_cdnc_ctw,-1);
     var fileStream_cdnc_wghts = FileSystem.inst().listFilesStream(pathToFiles_cdnc_wghts,-1);
     var fileStream_swrf = FileSystem.inst().listFilesStream(pathToFiles_swrf,-1);
-    var fileStream_extCoeff = FileSystem.inst().listFilesStream(pathToFiles_extCoeff,-1);
+    // var fileStream_extCoeff = FileSystem.inst().listFilesStream(pathToFiles_extCoeff,-1);
     var smokePPEFiles = new Array();
 
     while(fileStream.hasNext()) {
@@ -79,12 +79,12 @@ function upsertFileTable() {
         };
     };
 
-    while(fileStream_extCoeff.hasNext()) {
-        var file = fileStream_extCoeff.next();
-        if(file.url.endsWith(".nc")) {
-            smokePPEFiles.push(file);
-        };
-    };
+    // while(fileStream_extCoeff.hasNext()) {
+    //     var file = fileStream_extCoeff.next();
+    //     if(file.url.endsWith(".nc")) {
+    //         smokePPEFiles.push(file);
+    //     };
+    // };
 
     var fileObjects = smokePPEFiles.map(createSimOutFile);
     SppeSimulationEnsembleOutputFile.upsertBatch(fileObjects);
@@ -94,10 +94,14 @@ function upsertFileTable() {
     function createSimOutFile(file) {
         if (file.url.includes("azure://smoke-ppe/")) {
             var container = "smoke-ppe";
-            var filename = file.url.split("azure://smoke-ppe/")[1] + 'mod';
+            var filename = file.url.split("azure://smoke-ppe/")[1];
+            var id = filename + 'mod';
+            // parse the stashCode out of the filename, where the name is of the form "ens_#_..._<m01...>.nc"
+            var stashCode = filename.split("_").slice(-1)[0].split(".")[0];
             return SppeSimulationEnsembleOutputFile.make({
-                "id": filename,
+                "id": id,
                 "simulationRun": simSample,
+                "stashCode": stashCode,
                 "file": File.make({
                     "url": file.url
                 }),
