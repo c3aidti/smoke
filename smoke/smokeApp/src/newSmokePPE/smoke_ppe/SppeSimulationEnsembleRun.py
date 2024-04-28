06ad2ce4c3ab74c0ca0091810169ab37b8794262
Darren Adams <dadams@illinois.edu>
2024-04-28 08:11:17 -0500

    tray caching grid points in output type

--- smoke/smokeApp/src/newSmokePPE/smoke_ppe/SppeSimulationEnsembleRun.py
+++ smoke/smokeApp/src/newSmokePPE/smoke_ppe/SppeSimulationEnsembleRun.py
@@ -784,8 +784,8 @@ def upsertInterpOutputToFlightTracks(this,datasetId,stashId,flightDate,campaign)
     df_grid.drop(columns = ['m01s02i530_550nm','altitude','model_level_number'],inplace = True)
     
     # Upsert Grid locations
-    batch_records = df_grid.to_dict(orient="records")
-    getattr(c3, geoTimeGridType).mergeBatch(objs=batch_records)
+    # batch_records = df_grid.to_dict(orient="records")
+    # getattr(c3, geoTimeGridType).mergeBatch(objs=batch_records)
     
     df_output = iris_cubelist_to_dataframe(cubelist)
     
@@ -804,7 +804,8 @@ def upsertInterpOutputToFlightTracks(this,datasetId,stashId,flightDate,campaign)
     df_output['gridId'] = dataset.id +"_"+round(df_output["latitude"],3).astype(str) + "_" + round(df_output["longitude"],3).astype(str) + "_" + df_output["time"].astype(str).apply(lambda x: x.replace(" ", 'T'))
     df_output["gridId"] = df_output["gridId"].apply(hash_string)
     df_output["geoTimeGridPoint"] = df_output["gridId"].apply(make_gstp)
-    df_output.drop(columns = ['time','latitude','longitude','model_level_number','gridId'],inplace = True)
+    # df_output.drop(columns = ['time','latitude','longitude','model_level_number','gridId'],inplace = True)
+    df_output.drop(columns = ['model_level_number','gridId'],inplace = True)
     
     # upsert Interpolated Output
     batch_records = df_output.to_dict(orient="records")
